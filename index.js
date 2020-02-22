911f3438e2e12162e3d7efa4d34da1cb54bd8e2d
Asimov4 <asimov4@gmail.com>
2020-02-22 15:31:28 -0800

    First working commit of a call to NDC api

--- index.js
+++ index.js
@@ -1,18 +1,109 @@
 // content of index.js
-const http = require('http')
-const port = 3000
+const http = require('http');
+const request = require('request');
+const port = 3000;
 
-const requestHandler = (request, response) => {
-  console.log(request.url)
-  response.end('Hello Node.js Server!')
+const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<IATA_AirShoppingRQ xmlns="http://www.iata.org/IATA/2015/00/2019.2/IATA_AirShoppingRQ">
+    <MessageDoc>
+        <RefVersionNumber>1.0</RefVersionNumber>
+    </MessageDoc>
+    <Party>
+        <Participant>
+            <Aggregator>
+                <AggregatorID>88888888</AggregatorID>
+                <Name>JR TECHNOLOGIES</Name>
+            </Aggregator>
+        </Participant>
+        <Sender>
+            <TravelAgency>
+                <AgencyID>{{AGENCY_ID}}</AgencyID>
+                <IATA_Number>12312312</IATA_Number>
+                <Name>Gods Travel</Name>
+            </TravelAgency>
+        </Sender>
+    </Party>
+    <PayloadAttributes>
+        <EchoTokenText>{{$guid}}</EchoTokenText>
+        <Timestamp>2001-12-17T09:30:47+05:00</Timestamp>
+        <TrxID>transaction{{$randomInt}}</TrxID>
+        <VersionNumber>2019.2</VersionNumber>
+    </PayloadAttributes>
+    <POS>
+        <City>
+            <IATA_LocationCode>ATH</IATA_LocationCode>
+        </City>
+        <Country>
+            <CountryCode>GR</CountryCode>
+        </Country>
+        <RequestTime>2018-10-12T07:38:00</RequestTime>
+    </POS>
+    <Request>
+        <FlightCriteria>
+            <OriginDestCriteria>
+                <DestArrivalCriteria>
+                    <IATA_LocationCode>LHR</IATA_LocationCode>
+                </DestArrivalCriteria>
+                <OriginDepCriteria>
+                    <Date>2020-06-20</Date>
+                    <IATA_LocationCode>BCN</IATA_LocationCode>
+                </OriginDepCriteria>
+            </OriginDestCriteria>
+            <OriginDestCriteria>
+                <DestArrivalCriteria>
+                    <IATA_LocationCode>BCN</IATA_LocationCode>
+                </DestArrivalCriteria>
+                <OriginDepCriteria>
+                    <Date>2020-06-27</Date>
+                    <IATA_LocationCode>LHR</IATA_LocationCode>
+                </OriginDepCriteria>
+                <PreferredCabinType>
+                    <CabinTypeCode>M</CabinTypeCode>
+                </PreferredCabinType>
+            </OriginDestCriteria>
+        </FlightCriteria>
+        <Paxs>
+            <Pax>
+                <PaxID>Pax1</PaxID>
+                <PTC>ADT</PTC>
+            </Pax>
+            <Pax>
+                <PaxID>Pax2</PaxID>
+                <PTC>CHD</PTC>
+            </Pax>
+        </Paxs>
+        <ShoppingCriteria>
+            <CabinTypeCriteria>
+                <CabinTypeCode>M</CabinTypeCode>
+            </CabinTypeCriteria>
+        </ShoppingCriteria>
+    </Request>
+</IATA_AirShoppingRQ>`;
+
+const options = {
+    url: 'https://iata.api.mashery.com/athena/ndc192api',
+    headers: {
+        'Content-Type': 'application/xml',
+        'Authorization-Key': 'cfhjc5kthf44n23eft3qhum4',
+    },
+    body: xml,
+};
+
+const requestHandler = (req, res) => {
+    request.post(options, xml, function (error, response, body) {
+        console.error('error:', error); // Print the error if one occurred
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        console.log('body:', body); // Print the HTML for the Google homepage.
+        res.end(body);
+    });
+
 }
 
-const server = http.createServer(requestHandler)
+const server = http.createServer(requestHandler);
 
 server.listen(port, (err) => {
-  if (err) {
-    return console.log('something bad happened', err)
-  }
-
-  console.log(`server is listening on ${port}`)
+    if (err) {
+        return console.log('something bad happened', err);
+    }
+    console.log(`server is listening on ${port}`);
 })
